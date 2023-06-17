36c0bb83fe2598e1fd3d850248254ba8bfaec1af
Battugsaa <tugsking2002@gmail.com>
2023-06-17 21:25:30 +0800

    first commit

---
+++ src/app/components/Container.tsx
@@ -0,0 +1,11 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div className="max-w-[2520] mx-auto xl:px-20 sm:px-2 px-4">{children}</div>
+  );
+};
+
+export default Container;
